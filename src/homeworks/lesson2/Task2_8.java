7078e8c6ec140ed950243b401e7724523a3c7fe4
goharvardanyan <gohar.vardanyanc@gmail.com>
2020-05-31 19:18:48 +0400

    new solution

--- src/homeworks/lesson2/Task2_8.java
+++ src/homeworks/lesson2/Task2_8.java
@@ -13,25 +13,22 @@ public class Task2_8 {
         int radius = 5;
         double p = 3.1415926535897932;
         Scanner scanner = new Scanner(System.in);
-
-        if (!scanner.hasNextInt()) {
-            System.out.println("Wrong input");
-        } else {
-            int number = scanner.nextInt();
-            switch (number) {
-                case 1:
-                    double area = p * radius * radius;
-                    System.out.println("The area of the circle: " + area);
-                    break;
-                case 2:
-                    double perimeter = 2 * p * radius;
-                    System.out.println("The perimeter of the circle: " + perimeter);
-                    break;
-                case 3:
-                    break;
-                default:
-                    System.out.println("Wrong input");
+        int number = scanner.nextInt();
+        while (number != 3) {
+            if (number == 1) {
+                double area = p * radius * radius;
+                System.out.println("The area of the circle: " + area);
+            } else if (number == 2) {
+                double perimeter = 2 * p * radius;
+                System.out.println("The perimeter of the circle: " + perimeter);
+            } else {
+                System.out.println("Wrong input");
             }
+            System.out.println("------------MENU-----------\n" +
+                    "---> enter 1 for calculating area of the circle\n" +
+                    "---> enter 2 for calculating perimeter of the circle\n" +
+                    "---> enter 3 for exit.\n");
+            number = scanner.nextInt();
         }
     }
 }
